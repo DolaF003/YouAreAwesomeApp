e041cc68a5f1480a68c46672ac3e8d464a05194e
DolaF003 <fakeye@bc.edu>
2025-08-28 17:54:33 -0400

    After lesson 1.3

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,22 +12,41 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable() //stretches image out to ends of device
                 .scaledToFit() //normalizes stretched out image (but still bigger than before)
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200) //added an invisble frame, made it smaller 
+            
             Text(message)
                 .font(.largeTitle) //changed the size
-                .fontWeight(.heavy) //changed the boldness
-                .foregroundStyle(.red) //changed the color
-            Button("Click Me!") {
-                message = "Awesome!"
-            }
+                .fontWeight(.ultraLight) //changed the boldness
+                .foregroundStyle(.black) //changed the color
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
                 
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+                
+                
+                
+            }
+            .buttonStyle(.borderedProminent) //can place modifiers outside container if they do the same
+            //thing for multiple buttons. Makes it more efficient to edit
+            .font(.title2)
+            .tint(.orange)
+            
+            .padding()
+            
         }
-        
-        .padding()
-        
     }
 }
 
