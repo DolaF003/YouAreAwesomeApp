7ac3385e659c82c86f0fd7a89c25fe5dac19bdf2
DolaF003 <fakeye@bc.edu>
2025-09-02 13:27:25 -0400

    After lesson 1.7

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,50 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable() //stretches image out to ends of device
                 .scaledToFit() //normalizes stretched out image (but still bigger than before)
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .frame(width: 400, height: 400) //added this to normalize picture within screen
+                
             
             Text(message)
                 .font(.largeTitle) //changed the size
-                .fontWeight(.ultraLight) //changed the boldness
-                .foregroundStyle(.black) //changed the color
+                .fontWeight(.heavy) //changed the boldness
+                .foregroundStyle(.red)
             
             Spacer()
-            
             
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
                 
                 message = (message == message1 ? message2 :  message1)
-                imageString = (imageString == imageString1 ? imageString2 : imageString1)
-                
-                
-//                if message == message1 {
-//                    message = message2
-//                    imageString = imageString1
-//                } else {
-//                    message = message1
-//                    imageString = imageString2
-//                }
-                
+                imageName = (imageName == "image0" ? "image1" : "image0")
+            
             }
-            
-            
-            
+       
             .buttonStyle(.borderedProminent) //can place modifiers outside container if they do the same
             //thing for multiple buttons. Makes it more efficient to edit
             .font(.title2)
-            .tint(.orange)
             
             .padding()
             
