c980d1bdccfb126f52e65e52d64d7d00337dd2ae
DolaF003 <fakeye@bc.edu>
2025-09-15 13:22:10 -0400

    lesson 1.12 (running into some issues)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,25 +10,30 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNum = 0
+    @State private var lastMessageNumber = -1 //lastMessageNumber will never be -1
+    @State private var lastImageNumber = -1
+    let numberofImages = 10
+    
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle) //changed the size
+                .fontWeight(.heavy) //changed the boldness
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
             Image(imageName)
                 .resizable() //stretches image out to ends of device
                 .scaledToFit() //normalizes stretched out image (but still bigger than before)
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
                 .frame(width: 400, height: 400) //added this to normalize picture within screen
+                .animation(.default, value: imageName)
                 
-            
-            Text(message)
-                .font(.largeTitle) //changed the size
-                .fontWeight(.heavy) //changed the boldness
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
             
             Spacer()
             
@@ -38,28 +43,29 @@ struct ContentView: View {
                                 "You Are One of One!",
                                 "You are Doing Amazing!",
                                 "Who Betta Than?? Nobody!",
-                                "Gorgeous!"]
+                                "Gorgeous!", "You Are Amazing!",
+                                "GO YOU!!", "Truly Fantastic!",
+                                "GOD GOT YOU!"]
+                
+                var messageNumber: Int
+                
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
                 
-                message = messages[messageNum]
-                messageNum += 1
+                var imageNumber: Int
                 
-                if messageNum == messages.count {
-                    messageNum = 0
-                }
-                
-                
-                //TODO: - Update the imageName variable -
+               repeat {
+                    imageNumber = Int.random(in: 0...9)
+                }  while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
                 
-                imageNumber += 1
-                
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
             }
-            .buttonStyle(.borderedProminent) //can place modifiers outside container if they do the same
-            //thing for multiple buttons. Makes it more efficient to edit
+            .buttonStyle(.borderedProminent)
             .font(.title2)
             
         }
